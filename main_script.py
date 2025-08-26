962ef23dad50ed2316a1ac19cc01e01a7103f2df
denetpyqt5 <aiengineermain@gmail.com>
2025-08-26 22:25:15 +0300

    1.02

--- main_script.py
+++ main_script.py
@@ -1,11 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
 
-def get_info(collection,number,model):
+db = sqlite3.connect('main.db')
+cur = db.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS nft (number integer, model text)")
+
+def get_info(collection,number,model_):
     response = requests.get(f'https://t.me/nft/{collection}-{str(number)}')
     soup = BeautifulSoup(response.text, 'html.parser')
     list = soup.find('meta', property="og:description").get('content').split('\n')
-    print(number) if "Model: " + model in list else None
+    cur.execute(f"""INSERT INTO  nft VALUES ('{number}','{model_}')""") if "Model: " + model_ in list else None
+    db.commit()
 
 
 user_collection = input("Enter your collection: ")
@@ -21,3 +27,5 @@ supply = int(''.join(arr))
 
 for i in range(supply):
     get_info(user_collection,i,user_model)
+
+db.close()
